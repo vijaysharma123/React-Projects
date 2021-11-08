610e3328e2c92cf171d38c03b8d45a0f6317e6cc
Jigar Sable <jigarsable21@gmail.com>
2021-11-08 23:17:23 +0530

    todo added

--- basic-todo/src/App.js
+++ basic-todo/src/App.js
@@ -1,16 +1,48 @@
 import './App.css';
-import Searchbar from './Components/Searchbar';
+import { useState, useEffect } from 'react';
+import Inputbar from './Components/Inputbar';
 import Todolist from './Components/Todolist';
 
 function App() {
+
+  let todosArr;
+  if(localStorage.getItem("todos")===null){
+    todosArr = [];
+  } else {
+    todosArr = JSON.parse(localStorage.getItem("todos"));
+  }
+  
+  const [todos, setTodos] = useState(todosArr);
+
+  const addTodo = (todo) => {
+    // console.log(todo);
+    let id;
+    todos.length===0? id=0 : id=todos[todos.length-1].id + 1;
+    const newTodo = {
+      id: id,
+      title: todo
+    }
+    setTodos([...todos, newTodo]);
+  }
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo)=>{
+      return todo.id !== id;
+    }));
+  }
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }, [todos]);
+
   return (
     <>
       {/* search container */}
       <div className="md:w-1/2 mx-auto mt-20 text-center">
         <h1 className="font-medium text-3xl">React Todos</h1>
-        <Searchbar />
+        <Inputbar addTodo={addTodo}/>
 
-        <Todolist />
+        <Todolist todos={todos} deleteTodo={deleteTodo}/>
         </div>
     </>
   );
