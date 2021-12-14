72b98ef9f5e917501a83cfd0bc25f6dba5707ce2
Jigar Sable <jigarsable21@gmail.com>
2021-12-14 22:44:59 +0530

    recipe app added

--- contact-manager/src/components/Header.js
+++ contact-manager/src/components/Header.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from 'react'
-import { Button, Box, AppBar, Toolbar, Typography, Switch } from '@mui/material';
+import { Button, Box, AppBar, Toolbar, Typography, Switch, FormControlLabel } from '@mui/material';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import { Link } from "react-router-dom";
 
@@ -12,11 +11,16 @@ const Header = ({toggleTheme}) => {
           <Typography variant="h6" component="div" gutterBottom sx={{ flexGrow: 1 }}>
             <Link to="/" style={{ color: 'inherit', textDecoration: 'inherit' }}>Contact Manager</Link>
           </Typography>
+
           <Button sx={{ marginRight: 2 }} component={Link} to="/add" color="inherit" variant="outlined" startIcon={<AddCircleIcon />}>
           Add Contact
           </Button>
-          <Switch onChange={()=>toggleTheme()}></Switch>
-          <Typography variant="body">Dark Mode</Typography>
+
+          <FormControlLabel
+          control={<Switch onChange={()=>toggleTheme()}></Switch>}
+          label="Dark Mode"
+          />
+          
         </Toolbar>
       </AppBar>
     </Box>
---
+++ gh-users/src/App.js
@@ -0,0 +1,67 @@
+import './App.css';
+import { Container, Paper, Typography, Grid, Card, CardContent, CardMedia, CircularProgress, Skeleton, Button } from '@mui/material';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function App() {
+  
+  const [users, setUsers] = useState(null);
+
+  const getUsers = async () => {
+    const url = "https://api.github.com/users";
+    await axios.get(url)
+    .then((res) => (
+      // console.log(res.data)
+      setUsers(res.data)
+    ))
+  }
+
+  useEffect(() => {
+      getUsers();
+  }, [])
+
+  return (
+    <>
+      <Typography variant="h4" align="center" color="white" mt={4}>
+        GitHub Users
+      </Typography>
+
+        <Grid sx={{padding:6}} container justifyContent="center" spacing={4}>
+          {
+            users && (
+            users.map((user) => {
+              const { avatar_url, login, html_url } = user;
+              return (
+        <Grid item xs={12} sm={6} md={4} lg={3}>
+          <Card sx={{ maxWidth: "full" }}>
+        <CardMedia
+          component="img"
+          height="220"
+          image={avatar_url}
+          alt="profile avatar"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {login}
+          </Typography>
+          <a target="_blank" href={html_url} style={{ textDecoration: "none" }}><Button variant="contained" fullWidth>Follow</Button></a>
+        </CardContent>
+    </Card>
+        </Grid>
+            )})
+            )
+            
+          }
+
+          {
+          !users && (
+              <CircularProgress />
+            )
+          }
+
+        </Grid>
+    </>
+  );
+}
+
+export default App;
