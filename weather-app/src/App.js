0b6bf0bfdebae0b219f3d567dd8761bb7cfdd736
Jigar Sable <jigarsable21@gmail.com>
2021-12-29 18:26:47 +0530

    minor fixes

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,51 +1,51 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import './App.css';
-import Leftsidebar from './Components/Leftsidebar';
-import Rightsidebar from './Components/Rightsidebar';
+import Leftsidebar from './components/Leftsidebar';
+import Rightsidebar from './components/Rightsidebar';
 
 function App() {
-  
+
   const [city, setCity] = useState({});
   const [dailyWeatherData, setDailyWeatherData] = useState({});
 
   const fetchWeather = async (searchVal) => {
     try {
-      let uri = `https://api.openweathermap.org/data/2.5/weather?q=${searchVal}&units=metric&appid=b1307ae5386855405cb82858030eaece`
+      let uri = `https://api.openweathermap.org/data/2.5/weather?q=${searchVal}&units=metric&appid=${process.env.REACT_APP_API_KEY}`
       await axios.get(uri)
-      .then((res) => {
-      console.log(res.data);
+        .then((res) => {
+          // console.log(res.data);
 
-      const { temp, humidity, pressure } = res.data.main;
-      const { main: weathermood } = res.data.weather[0];
-      const { name, dt } = res.data;
-      const  { lat, lon } = res.data.coord;
-      const { speed } = res.data.wind;
-      const { country, sunset } = res.data.sys;
+          const { temp, humidity, pressure } = res.data.main;
+          const { main: weathermood } = res.data.weather[0];
+          const { name, dt } = res.data;
+          const { lat, lon } = res.data.coord;
+          const { speed } = res.data.wind;
+          const { country, sunset } = res.data.sys;
 
-      const weatherInfo = { temp, humidity, pressure, weathermood, name, speed, country, sunset, lat, lon, dt };
+          const weatherInfo = { temp, humidity, pressure, weathermood, name, speed, country, sunset, lat, lon, dt };
 
-      setCity(weatherInfo);
-      fetchDailyData(weatherInfo.lat, weatherInfo.lon);
-    });
-  
+          setCity(weatherInfo);
+          fetchDailyData(weatherInfo.lat, weatherInfo.lon);
+        });
+
     } catch (error) {
       console.log(error);
-      alert("No Results Found!");
+      // alert("No Results Found!");a
     }
 
   }
 
   const fetchDailyData = async (lat, lon) => {
     try {
-      let uri = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely&units=metric&appid=b1307ae5386855405cb82858030eaece`;
+      let uri = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely&units=metric&appid=${process.env.REACT_APP_API_KEY}`;
       await axios.get(uri)
-      .then((res) => {
-        console.log(res.data);
-        const { lat, lon, daily } = res.data;
-        const newInfo = { lat, lon, daily }
-        setDailyWeatherData(newInfo);
-      });
+        .then((res) => {
+          // console.log(res.data);
+          const { lat, lon, daily } = res.data;
+          const newInfo = { lat, lon, daily }
+          setDailyWeatherData(newInfo);
+        });
     } catch (error) {
       console.log(error)
     }
@@ -53,18 +53,18 @@ function App() {
   }
 
   useEffect(() => {
-    fetchWeather("Pune");    
+    fetchWeather("Pune");
     fetchDailyData(18.5196, 73.8553);
   }, []);
 
   return (
-      <div className="flex mx-auto bg-gray-800 rounded shadow">
+    <div className="flex mx-auto bg-gray-800 rounded shadow">
 
-        <Leftsidebar city={city} />
+      <Leftsidebar city={city} />
 
-        <Rightsidebar dailyWeatherData={dailyWeatherData} fetchWeather={fetchWeather} city={city} />
-        
-      </div>
+      <Rightsidebar dailyWeatherData={dailyWeatherData} fetchWeather={fetchWeather} city={city} />
+
+    </div>
   );
 }
 
--- weather-app/src/Components/Leftsidebar.js
+++ weather-app/src/Components/Leftsidebar.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react'
 
-export default function Leftsidebar({ city }) { 
+export default function Leftsidebar({ city }) {
 
     const [weatherIcon, setweatherIcon] = useState("");
 
     useEffect(() => {
-        if(city.weathermood) {
-            switch(city.weathermood) {
+        if (city.weathermood) {
+            switch (city.weathermood) {
                 case "Clouds":
                     setweatherIcon("wi-day-cloudy");
                     break;
@@ -30,15 +30,15 @@ export default function Leftsidebar({ city }) {
 
     return (
         <div className="w-1/2 py-6 px-8 pr-24 bg-indigo-600 rounded-l">
-          <h1 className="text-3xl font-medium">{getDay()}</h1>
-          <h2>{new Date().toLocaleDateString()}</h2>
-          <h2 className="mt-2"><i className="fas fa-map-marker-alt"></i> { city.name }, { city.country }</h2>
+            <h1 className="text-3xl font-medium">{getDay()}</h1>
+            <h2>{new Date().toLocaleDateString()}</h2>
+            <h2 className="mt-2"><i className="fas fa-map-marker-alt"></i> {city.name}, {city.country}</h2>
 
-          <div className="flex flex-col mt-20">
-          <i className={`wi ${weatherIcon} text-5xl`}></i>
-          <span className="text-4xl font-bold mt-5">{ city.temp }°C</span>
-          <p className="font-medium text-lg">{ city.weathermood }</p>
-          </div>
+            <div className="flex flex-col mt-20">
+                <i className={`wi ${weatherIcon} text-5xl`}></i>
+                <span className="text-4xl font-bold mt-5">{city.temp}°C</span>
+                <p className="font-medium text-lg">{city.weathermood}</p>
+            </div>
         </div>
     )
 }
